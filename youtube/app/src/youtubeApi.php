acf9d05a89df67b26468f87a6c15c173fce724a0
takayuki.miura <t.miura0203@gmail.com>
2019-04-30 20:29:12 +0900

    add youtube api process

---
+++ youtube/app/src/youtubeApi.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+
+class YoutubeApi
+{
+    function run() {
+        $client = new Google_Client();
+        $client->setApplicationName('Sample Application');
+        // Note: Replace to your key
+        $client->setDeveloperKey('xxxx');
+    
+        $youtube = new Google_Service_YouTube($client);
+    
+        // get search result
+        $searchResponse = $youtube->search->listSearch(
+            'snippet',
+            [
+                'q' => 'Vtuber',
+                'type' => 'channel',
+                'type' => 'channel',
+                'order' => 'viewCount',
+                'maxResults' => 10,
+                'regionCode' => 'JP'
+            ]
+        );
+
+        foreach($searchResponse->items as $item) {
+            // get channel info
+            $channelsResponse = $youtube->channels->listChannels(
+                'statistics',
+                ['id' => $item->snippet->channelId]
+            );
+
+            echo('チャンネル名：' . $item->snippet->channelTitle);
+            echo("\n");
+            echo('登録者数：' . $channelsResponse->items[0]->statistics->subscriberCount);
+            echo("\n");
+            echo('総再生数：' . $channelsResponse->items[0]->statistics->viewCount);
+            echo("\n");
+            echo("----\n\n");
+        }
+    }
+}
+
+$youtubeApi = new YoutubeApi();
+$youtubeApi->run();
